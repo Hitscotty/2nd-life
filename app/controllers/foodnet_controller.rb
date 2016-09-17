273b3bb00bb614aab54423658e7d2a8b4f78eb56
Jonathan <Jportorreal77@gmail.com>
2016-09-17 01:03:28 -0400

    parsing json

--- app/controllers/foodnet_controller.rb
+++ app/controllers/foodnet_controller.rb
@@ -1,5 +1,6 @@
 require 'httparty'
 require 'ckan'
+require 'json'
 
 REST_API = "http://demo.ckan.org/api/3/action"
 AUTH_KEY = "upRQngIuH8UPWVrpJzZkadM2OipwO0y6oKBqXxR8"
@@ -12,26 +13,38 @@ class FoodnetController < ApplicationController
   def net
      #@data = searchTag(SAMPLE_TAG)
      #@data = searchSubTags(SAMPLE_TAG, SAMPLE_SUB_TAG)
-     @data = topTags()
+     jsdata = searchTag("nutrition")
+     data = verify(jsdata)
+     @data = data
+     puts jsdata["result"][data[2]]
   end
+end
+
+
+def verify(dict)
+ if dict["success"]
+  return dict["result"].keys
+ else
+  return {}
+ end
 end
 
 # ckan API actions aka my very own client
 def getTags
- return HTTParty.get(REST_API + '/tag_list')
+ return HTTParty.get(REST_API + '/tag_list', {format: :json})
 end
 
 # returns list of top data sets
 def topTags
- return HTTParty.get(REST_API + '/package_search?facet.field=[%22tags%22]&facet.limit=' + NUM_OF_TAGS + '&rows=0')
+ return HTTParty.get(REST_API + '/package_search?facet.field=[%22tags%22]&facet.limit=' + NUM_OF_TAGS + '&rows=0', {format: :json})
 end
 
 # search all datasets for given tag
 def searchTag(tag)
- return HTTParty.get(REST_API + '/package_search?fq=tags:' + tag)
+ return HTTParty.get(REST_API + '/package_search?fq=tags:' + tag, {format: :json})
 end
 
 # A list of datasets using subtag from vocabulary tag:
 def searchSubTags(tag, subtag)
- return HTTParty.get('https://data.hdx.rwlabs.org/api/3/action/package_search?fq=vocab_' + tag + ':' + subtag)
+ return HTTParty.get('https://data.hdx.rwlabs.org/api/3/action/package_search?fq=vocab_' + tag + ':' + subtag, {format: :json})
 end
